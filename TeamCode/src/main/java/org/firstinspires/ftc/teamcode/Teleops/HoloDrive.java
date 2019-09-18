947cc038e1ac3c6e186f6287a99ec27abfc888cf
david-wiles6 <31745593+david-wiles6@users.noreply.github.com>
2019-09-18 13:59:24 -0400

    Updated Holo drive to be able to work
    - Added encoder example code
    - Allowed Holo drive to spin while moving
    - Cool and gûd stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/HoloDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/HoloDrive.java
@@ -19,43 +19,37 @@ public class HoloDrive extends LinearOpMode{
         float rightLeftPos;
         float upDownPos;
         float rightPos;
-        double sinAngle;
-        double cosAngle;
-        boolean leftMotorBoole;
-        boolean rightMotorBoole;
-        boolean frontMotorBoole;
-        boolean backMotorBoole;
+        float leftPower;
+        float rightPower;
+        float frontPower;
+        float backPower;
+        int frontPos;
         //tell the phones you are ready
         telemetry.addLine("Ready to Run!");
         telemetry.update();
+        robot.backMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
         while(opModeIsActive()){
-            /*PseudoCode
-            find the
-            assign to each motor
-             */
-            rightLeftPos  = (gamepad1.left_stick_x);
-            upDownPos = (gamepad1.left_stick_y);
-            rightPos = (gamepad1.right_stick_x);
-            /*
-            robot.rightMotor.setPower(upDownPos);
-            robot.leftMotor.setPower(-1*upDownPos);
-            robot.backMotor.setPower(rightLeftPos);
-            robot.frontMotor.setPower(-1*rightLeftPos);
-            */
-            //insert backend/frontend meme here
-            leftMotorBoole = gamepad1.left_stick_x<0;
-            rightMotorBoole = gamepad1.left_stick_x>0;
-            frontMotorBoole = gamepad1.left_stick_y>0;
-            backMotorBoole = gamepad1.left_stick_y<0;
-            if (leftMotorBoole) robot.leftMotor.setPower(upDownPos);
-
-            if (rightMotorBoole) robot.rightMotor.setPower(-1 * upDownPos);
-
-            if (frontMotorBoole) robot.backMotor.setPower(rightLeftPos);
-
-            if (backMotorBoole) robot.frontMotor.setPower(rightLeftPos);
-            spinSpeed = (gamepad1.right_stick_x);
+            //get the position from the joysticks
+            rightLeftPos  = (gamepad1.left_stick_x)*9/10;
+            upDownPos = (gamepad1.left_stick_y)*9/10;
+            spinAdjust = (gamepad1.right_stick_x)/10;
+            //Add the direction and the spin amount to get drive
+            rightPower = upDownPos+spinAdjust;
+            leftPower = spinAdjust-upDownPos;
+            frontPower = rightLeftPos+spinAdjust;
+            backPower = spinAdjust-rightLeftPos;
+            //set the power to the motor
+            robot.rightMotor.setPower(rightPower);
+            robot.leftMotor.setPower(leftPower);
+            robot.backMotor.setPower(backPower);
+            robot.frontMotor.setPower(frontPower);
+            
+            frontPos = frontMotor.getCurrentPosition();
+            telemetry.addData("Front Position"+frontPos);
 
 
         }
